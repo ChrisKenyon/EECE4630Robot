e507f297f6d4111d83ab54f02f647d4cbf433d92
Kj Staley <kstaley22594@gmail.com>
2017-03-22 19:40:34 -0400

    Line following basically done, more tuning might be needed based on course

--- LineFollow/LineFollow.ino
+++ LineFollow/LineFollow.ino
@@ -2,7 +2,17 @@
 #include <RedBotSoftwareSerial.h>
 #include <PID_v1.h>
 
-#define MAX_SPEED 90
+#define MAX_SPEED 110
+// Experimentally determined, most line readings are in range [850, 950] 
+// most wood readings are in range [50, 550]
+#define THRESHOLD 800
+
+// States
+#define S_STRAIGHT        0
+#define S_STRAIGHT_LEFT   1
+#define S_STRAIGHT_RIGHT  2
+#define S_LEFT            3
+#define S_RIGHT           4
 
 #define PIN_LINE_LEFT  A0
 #define PIN_LINE_MID   A1
@@ -27,12 +37,15 @@ void setMotors(double left, double right) {
   if (right * MAX_SPEED > 255) {
     right = 255.0/MAX_SPEED;
   }
-  motors.rightMotor(0 - int(MAX_SPEED * right));
   motors.leftMotor(0 - int(MAX_SPEED * left));
+  motors.rightMotor(0 - int(MAX_SPEED * right));
 }
 
+double leftreading, rightreading;
 double getError() {
-  return (double)(analogRead(PIN_LINE_LEFT)-analogRead(PIN_LINE_RIGHT));
+  leftreading = (double)analogRead(PIN_LINE_LEFT);
+  rightreading = (double)analogRead(PIN_LINE_RIGHT);
+  return leftreading - rightreading;
 }
 
 void setup(){
@@ -42,13 +55,14 @@ void setup(){
   //turn the PID on
   pid.SetMode(AUTOMATIC);
   pid.SetOutputLimits(-1,1);
-  pid.SetSampleTime(5);
+  pid.SetSampleTime(2);
 
   Serial.print("Setup Complete.\n");
 }
 
 bool compute = false;
 double error = 0;
+int state = 0;
 
 void loop() {
 
@@ -56,10 +70,55 @@ void loop() {
   while (compute == false) {
     error = getError();
     pidIn = error;
-    Serial.print(error);
-    Serial.print("\n");
     compute = pid.Compute();
   }
-
-  setMotors(1+pidOut, 1-pidOut);
+  
+  switch (state) {
+    case S_STRAIGHT:
+      setMotors(1+pidOut, 1-pidOut);
+      if (leftreading > THRESHOLD && rightreading > THRESHOLD) {
+        // random turn
+      }
+      else if (leftreading > THRESHOLD) {
+        state = S_STRAIGHT_RIGHT;
+      }
+      else if (rightreading > THRESHOLD) {
+        state = S_STRAIGHT_LEFT;
+      }
+      break;
+      
+    case S_STRAIGHT_LEFT: // straightaway, left adjustment
+      setMotors(1+pidOut, 1-pidOut);
+      if (leftreading > THRESHOLD && rightreading > THRESHOLD) {
+        state = S_LEFT;
+      }
+      else if (leftreading < THRESHOLD) {
+        state = S_STRAIGHT;
+      }
+      break;
+    
+    case S_STRAIGHT_RIGHT: // straightaway, right adjustment
+      setMotors(1+pidOut, 1-pidOut);
+      if (leftreading > THRESHOLD && rightreading > THRESHOLD) {
+        state = S_RIGHT;
+      }
+      else if (rightreading < THRESHOLD) {
+        state = S_STRAIGHT;
+      }
+      break;
+     
+    case S_LEFT: // hard left turn
+      setMotors(-0.3, 1);
+      if (leftreading < THRESHOLD) {
+        state = S_STRAIGHT;
+      }
+      break;
+   
+    case S_RIGHT: // hard right turn
+      setMotors(1, -0.3);
+      if (rightreading < THRESHOLD) {
+        state = S_STRAIGHT;
+      }
+      break;  
+  } 
 }
