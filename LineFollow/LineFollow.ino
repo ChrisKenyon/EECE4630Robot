afc55333720696d5de20edfaa34fd2b5d9d9fea0
Nicholas R Tosta <tosta.n@northeastern.edu>
2017-03-22 17:10:20 -0400

    Refactor Line Following
    Refactored the LineFollow program so that it matches the WallFollowing
    algorith, need to be tested.

--- LineFollow/LineFollow.ino
+++ LineFollow/LineFollow.ino
@@ -1,75 +1,63 @@
 #include <RedBot.h>
 #include <RedBotSoftwareSerial.h>
 #include <PID_v1.h>
-#include <NewPing.h>
-
-#define LINE_IN1 A0
-#define LINE_IN2 A1
-#define LINE_IN3 A6
 
 #define MAX_SPEED 100
 
-RedBotMotors motors; // Instantiate the motor control object.
+#define PIN_LINE_LEFT  A0
+#define PIN_LINE_MID   A1
+#define PIN_LINE_RIGHT A6
 
-//Define Variables we'll be connecting to
-double Setpoint, in1, in2, in3, Output, pidIn;
-//Specify the links and initial tuning parameters
+// Want the left and right sensors to read the same value
+#define SETPOINT 0
+
+// Redbot motor(s) object
+RedBotMotors motors;
+
+// PID object and parameters
+double Setpoint = SETPOINT, pidOut, pidIn;
 double Kp=0.001, Ki=0, Kd=0.0001;
-PID pid(&pidIn, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID pid(&pidIn, &pidOut, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+// Takes doubles for left and right as percentage 0.0 - 1.0
+void setMotors(double left, double right) {
+  if (left * MAX_SPEED > 255) {
+    left = 255.0/MAX_SPEED;
+  }
+  if (right * MAX_SPEED > 255) {
+    right = 255.0/MAX_SPEED;
+  }
+  motors.rightMotor(0 - int(MAX_SPEED * right));
+  motors.leftMotor(0 - int(MAX_SPEED * left));
+}
+
+double getError() {
+  return (double)(analogRead(PIN_LINE_LEFT)-analogRead(PIN_LINE_RIGHT));
+}
 
 void setup(){
   Serial.begin(9600);
   Serial.print("Setup Started.\n");
-  //initialize the variables we're linked to
-  in1 = analogRead(LINE_IN1);
-  //in2 = analogRead(LINE_IN2);
-  in3 = analogRead(LINE_IN3);
-  pidIn = in3 - in1;
-  Setpoint = 0;
 
   //turn the PID on
   pid.SetMode(AUTOMATIC);
-  pid.SetOutputLimits(-1,1);
-  pid.SetSampleTime(50);
+  pid.SetOutputLimits(-0.5,0.5);
+  pid.SetSampleTime(20);
+
   Serial.print("Setup Complete.\n");
 }
 
-void setMotors(double pidOut){
-  if (pidOut > 0) {
-    motors.rightMotor(MAX_SPEED);
-    motors.leftMotor(MAX_SPEED * (1 - pidOut));
-  }
-  else {
-    motors.rightMotor(MAX_SPEED * (1 + pidOut));
-    motors.leftMotor(MAX_SPEED);
-  }
-}
+bool compute = false;
+double error = 0;
 
-void loop(){
-  //delay(50);
-  in1 = analogRead(LINE_IN1);
-  //in2 = analogRead(LINE_IN2);
-  in3 = analogRead(LINE_IN3);
-  /*
-  Serial.print("Input 1: ");
-  Serial.print(in1);
-  Serial.println("");
-  Serial.print("Input 3: ");
-  Serial.print(in3);
-  Serial.println("");
-  */
-  pidIn = in3 - in1;
-  /*
-  Serial.print("PID Input: ");
-  Serial.print(pidIn);
-  Serial.println("");
-  */
-  pid.Compute();
-  /*
-  Serial.print("PID response: ");
-  Serial.print(Output);
-  Serial.println("");
-  */
-  setMotors(Output);
-}
+void loop() {
 
+  compute = false;
+  while (compute == false) {
+    error = getError();
+    pidIn = error;
+    compute = pid.Compute();
+  }
+
+  setMotors(1-pidOut, 1+pidOut);
+}
